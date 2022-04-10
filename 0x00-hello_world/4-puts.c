e7c4f2638d70978c2ecdaebcb411ae01ae54d938
Olawalesegun <olawalesegun0@gmail.com>
2022-04-10 02:29:50 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
- * main - entry
- * Return: 0 after printing the function
+ * main - entry point
+ * return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-gcc 4-puts.c -o 4-puts
-./4-puts
