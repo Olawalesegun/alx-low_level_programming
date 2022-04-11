673cf8747a34dfbe71f960d8d701817b7ff61059
Olawalesegun <olawalesegun0@gmail.com>
2022-04-11 06:51:14 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+nclude <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
