2e7d45739b526d33b3e97fb60591b88fde0ecd8b
Olawalesegun <olawalesegun0@gmail.com>
2022-04-12 16:16:55 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - Entry                                                                                                                                        
+ *
+ * return - Always success
+ *
+ */
+int _putchar(void)
+{
+int;
+putchar("\n");
+return (0)                                                                                                                                             
+}
