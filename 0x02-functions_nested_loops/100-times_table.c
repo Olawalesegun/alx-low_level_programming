fb9ad1156cf1180463bcd0d718b110f3a731fe70
Olawalesegun <olawalesegun0@gmail.com>
2022-04-13 16:10:34 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -53,37 +53,23 @@ void print_times_table(int n)
 					else if (res >= 10 && res < 100)
 
 					{
-
 						_putchar(' ');
-
 						_putchar((res / 10) + '0');
 
 						_putchar((res % 10) + '0');
-
 					}
-
 					else if (res >= 100 && j != 0)
-
 					{
-
 						_putchar((res / 100) + '0');
 
 						_putchar((res / 10) % 10 + '0');
 
 						_putchar((res % 10) + '0');
-
 					}
-
 					else
-
 						_putchar((res % 10) + '0');
-
 				}
-
 				_putchar('\n');
-
 			}
-
 		}
-
 }
