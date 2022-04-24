9a6dbc35c78158c7e70c2b44b5d96f63250b8cd8
Olawalesegun <olawalesegun0@gmail.com>
2022-04-24 04:41:39 -0700

    0-strcat Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: second string to be concatenated to src
- * @src: first string to be concatenated to dest
- * Return: pointer to the resulting string dest
+ *_strcat - concatenates two strings
+ *@dest: second string to be concatenated to src
+ *@src: first string to be concatenated to dest
+ *Return: pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
