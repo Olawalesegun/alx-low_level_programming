29d5e3d719a258f26709a836429cd804ff0868a6
Olawalesegun <Olawalesegun0@gmail.com>
2022-07-03 08:14:04 -0700

    a C program that prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
- * main - Prints Programming is like building a multilingual puzzle
- * return: 0 after printing the function
+/**
+ * main - entry point
+ *
+ * return: 0 after printing the fuction
  */
 int main(void)
 {
