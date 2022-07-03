5b46a4facd534ae35c8c3e9f3f64313981bbc7ee
Olawalesegun <Olawalesegun0@gmail.com>
2022-07-03 08:06:22 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-/**
- *	main - Prints Programming is like building a multilingual puzzle
- *
- *	return: 0 after printing the function
+/*
+ * main - Prints Programming is like building a multilingual puzzle
+ * return: 0 after printing the function
  */
 int main(void)
 {
