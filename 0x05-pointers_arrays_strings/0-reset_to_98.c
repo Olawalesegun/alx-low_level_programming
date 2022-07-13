dc949142e03636ddd56b90ee45b6062b92e56e2b
Olawalesegun <Olawalesegun0@gmail.com>
2022-07-12 23:28:03 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 /**
-*reset_to_58 - takes  aponter to anint parameter
-*	and update its value its point to 98
-*
-*  @n: An integer that inputs pointer
-*
-* Return - Nothing
-*/
+ * reset_to_98 - takes  aponter to anint parameter
+ * and update its value its point to 98
+ *
+ * @n: An integer that inputs pointer
+ *
+ * Return - Nothing
+ */ 
 
 void reset_to_98(int *n)
 {
