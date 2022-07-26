4ed6d31743b0e2a3ddc4107592c69a9960b7d349
Olawalesegun <Olawalesegun0@gmail.com>
2022-07-26 10:09:43 -0700

    a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
-/**
- * _strdup - Duplicate a string using malloc
- * str: string to duplicate
- * Return: Pointer to a the new duped string
- */
-
+ 
 char *_strdup(char *str)
 {
 	char *a;
